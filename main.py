f2ed363641b0b49d964f1cde8c8194b4147deb6c
Tyclonie <tyclonie@gmail.com>
2025-02-12 22:10:41 +0000

    Accessed data from packets. MAC Addresses However

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from scapy.all import *
 
-
 class App:
     def __init__(self):
         self.packets = rdpcap("./resources/example_export.pcapng")
@@ -13,7 +12,7 @@ def main():
     app = App()
     packets = app.get_packets()
     for packet in packets:
-        print(packet)
+        print(f"{packet.src} -> {packet.dst}")
 
 
 if __name__ == '__main__':
