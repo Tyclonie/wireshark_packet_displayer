1bcc1db0903d9f191176987f08dc4999737d3b5b
Tyclonie <tyclonie@gmail.com>
2025-03-17 18:10:04 +0000

    Successfully generates lines from packet origin to the packet destination

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import socket
 from scapy.all import *
 from scapy.layers.inet import IP
 import requests
@@ -10,20 +11,36 @@ class GUI(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-    def load(self, lat, lon):
-        values = (400 + (lon * (20/9)) if lon > 0 else 400 - (-lon * (20/9)),
-                  203 + (-lat * (203/90)) if lat < 0 else 203 - (lat * (203/90)))
+    def load(self, ip_address_data, ip_map, local_information):
         self.geometry("800x406")
         image = Image.open("resources/map_of_earth.png")
         background_image = ImageTk.PhotoImage(image)
         canvas = customtkinter.CTkCanvas(self, width=800, height=406)
         canvas.pack()
         canvas.create_image(0, 0, image=background_image, anchor=tkinter.NW)
-        canvas.create_line(values[0], values[1], 800, 406, fill="purple", width=3)
+        for ip_set in ip_map:
+            try:
+                src_lon, src_lat = ip_address_data[ip_set[0]]['lon'], ip_address_data[ip_set[0]]['lat']
+                if ip_set[1] == local_information[1]:
+                    print(True)
+                    dst_lon, dst_lat = local_information[0]['lon'], local_information[0]['lat']
+                else:
+                    dst_lon, dst_lat = ip_address_data[ip_set[1]]['lon'], ip_address_data[ip_set[0]]['lat']
+                start_values = (400 + (src_lon * (20/9)) if src_lon > 0 else 400 - (-src_lon * (20/9)),
+                                203 + (-src_lat * (203/90)) if src_lat < 0 else 203 - (src_lat * (203/90)))
+                end_values = (400 + (dst_lon * (20 / 9)) if dst_lon > 0 else 400 - (-dst_lon * (20 / 9)),
+                              203 + (-dst_lat * (203 / 90)) if dst_lat < 0 else 203 - (dst_lat * (203 / 90)))
+                canvas.create_line(start_values[0], start_values[1], end_values[0], end_values[1], fill="purple",
+                                   width=3)
+                print(f"Data for {ip_set}")
+            except KeyError:
+                print(f"No data for {ip_set}")
         self.mainloop()
 
 class App:
     def __init__(self):
+        self.user_information = None
+        self.local_ip = None
         self.fail_count = None
         self.packets = rdpcap("resources/example_export2.pcapng")
         self.ip_addresses = []
@@ -66,17 +83,29 @@ class App:
     def get_ip_information(self) -> dict:
         return self.ip_addresses_data
 
+    def get_ip_map(self) -> list:
+        return self.ip_map
+
+    def get_local_ip_information(self) -> tuple:
+        self.user_information = requests.get("http://ip-api.com/json/?fields=18548473").json()
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(('8.8.8.8', 80))
+            self.local_ip = s.getsockname()[0]
+        except Exception:
+            self.local_ip = '127.0.0.1'
+        finally:
+            s.close()
+            return self.user_information, self.local_ip
+
 
 
 def main():
     app = App()
     app.add_ip_addresses()
-    print(app.ip_map)
-    #app.fetch_ip_information()
-    #print(app.get_ip_information())
-    #print(app.fail_count)
-    #gui = GUI()
-    #gui.load(72, -42)
+    app.fetch_ip_information()
+    gui = GUI()
+    gui.load(app.get_ip_information(), app.get_ip_map(), app.get_local_ip_information())
 
 
 
